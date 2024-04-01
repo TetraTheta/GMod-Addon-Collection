7af705d7f6e9cd01967755a351b9ccae6acfea0d
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2024-04-01 16:14:44 +0900

    Add 'Fix Map' This add-on will be part of other add-ons

---
+++ Fix Map/lua/autorun/server/fixmap_tfj_overflow.lua
@@ -0,0 +1,19 @@
+local function fixmap_tfj_overflow()
+  if game.GetMap() == "overflow" then
+    local mathCounter = ents.FindByName("bridge_wave2_counter")[1]
+    mathCounter:Input("AddOutput", mathCounter, nil, "OnHitMax apc01_bullseye,Break,,11,1")
+    mathCounter:Input("AddOutput", mathCounter, nil, "OnHitMax apc01_driver,SetRelationship,!player D_HT 9999,11,1")
+    mathCounter:Input("AddOutput", mathCounter, nil, "OnHitMax apc01_bullseye,Kill,,11,1")
+    mathCounter:Input("AddOutput", mathCounter, nil, "OnHitMax sniper_relay_zoomout,Trigger,,11,1")
+    mathCounter:Input("AddOutput", mathCounter, nil, "OnHitMax combine_mine_counter,Enable,,11,1")
+    mathCounter:Input("AddOutput", mathCounter, nil, "OnHitMax sniper_wall_break,Break,,11,1")
+    mathCounter:Input("AddOutput", mathCounter, nil, "OnHitMax sniper_wall_break_snd,PlaySound,,11,1")
+    mathCounter:Input("AddOutput", mathCounter, nil, "OnHitMax sniper_boards,Break,,11,1")
+    mathCounter:Input("AddOutput", mathCounter, nil, "OnHitMax bridge01_playerclip,KillHierarchy,,11,1")
+    mathCounter:Input("AddOutput", mathCounter, nil, "OnHitMax sniper_boards_front,Break,,11,1")
+    mathCounter:Input("AddOutput", mathCounter, nil, "OnHitMax sniper_tank,Deactivate,,11.01,1")
+    mathCounter:Input("AddOutput", mathCounter, nil, "OnHitMax sniper_tank,Kill,,11.1,1")
+    mathCounter:Input("AddOutput", mathCounter, nil, "OnHitMax sniper_controlvolume,Kill,,11.1,1")
+  end
+end
+hook.Add("InitPostEntity", "FixMap_TFJ_overflow", fixmap_tfj_overflow)
