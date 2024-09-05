e5489114cc74d2810f22ce54aff7f3ace17506fe
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2024-09-05 18:05:22 +0900

    [Private Reserve] Increase MP5SD damage

--- Private Reserve/lua/autorun/server/special_damage.lua
+++ Private Reserve/lua/autorun/server/special_damage.lua
@@ -1,17 +1,39 @@
-hook.Add("EntityTakeDamage", "SpecialDamage", function(e, dmg)
-  if not e:IsValid() then return end
-  local attacker = NULL
+---@param dmg CTakeDamageInfo
+local function _CheckPlayer(dmg)
   if dmg:GetAttacker():IsValid() and dmg:GetAttacker():GetClass() == "player" then
-    attacker = dmg:GetAttacker()
+    return dmg:GetAttacker()
   else
-    return
+    return NULL
   end
-  if attacker:IsValid() and attacker:GetActiveWeapon():IsValid() and attacker:GetActiveWeapon():GetClass() == "scw_mp5sd" then
+end
+
+---@param e Entity
+local function _CheckWeapon(e)
+  ---@cast e NPC
+  if e:IsValid() and (e:IsNPC() or e:IsPlayer()) and e:GetActiveWeapon():IsValid() then
+    return e:GetActiveWeapon()
+  else
+    return NULL
+  end
+end
+
+---@param e Entity
+---@param dmg CTakeDamageInfo
+hook.Add("EntityTakeDamage", "SpecialDamage", function(e, dmg)
+  -- Sanitize attacker and weapon
+  if not e:IsValid() then return end
+  local attacker = _CheckPlayer(dmg)
+  local weapon = _CheckWeapon(attacker)
+  -- Process damage
+  if weapon:IsValid() and weapon:GetClass() == "scw_mp5sd" then
+    -- MP5SD
     local cls = e:GetClass()
     if cls == "npc_manhack" then
       dmg:SetDamage(1000)
     elseif cls == "item_item_crate" then
       dmg:SetDamage(1000)
+    else
+      dmg:SetDamage(dmg:GetDamage() * 2)
     end
   else
     return
