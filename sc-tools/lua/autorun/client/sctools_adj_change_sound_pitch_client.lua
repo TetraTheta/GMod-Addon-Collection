7efd4a79ca8d666e8e17f9d8e75a86f7f4d9a2be
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2025-05-12 20:08:10 +0900

    fix(sc-tools): Fix sound issue
    Fix sound not played when 'sc_change_sound_pitch' is not set

--- sc-tools/lua/autorun/client/sctools_adj_change_sound_pitch_client.lua
+++ sc-tools/lua/autorun/client/sctools_adj_change_sound_pitch_client.lua
@@ -1,11 +1,10 @@
-local e_GetDemoPlaybackTimeScale = engine.GetDemoPlaybackTimeScale
-local m_Clamp = math.Clamp
---
 ---@param t EmitSoundInfo
 hook.Add("EntityEmitSound", "SCTOOLS_SoundPitchClient", function(t)
-  local scale = e_GetDemoPlaybackTimeScale()
-  if scale ~= 1 and GetConVar("sc_change_sound_pitch"):GetBool() then
-    t.Pitch = m_Clamp(t.Pitch * scale, 0, 255)
+  local cv_sc_pitch = GetConVar("sc_change_sound_pitch")
+  if cv_sc_pitch == nil then return end
+
+  if (engine.GetDemoPlaybackTimeScale() ~= 1 and cv_sc_pitch:GetBool()) then
+    t.Pitch = math.Clamp(t.Pitch * engine.GetDemoPlaybackTimeScale(), 0, 255)
     return true
   end
 end)
--- sc-tools/lua/autorun/server/sctools_adj_change_sound_pitch.lua
+++ sc-tools/lua/autorun/server/sctools_adj_change_sound_pitch.lua
@@ -1,17 +1,20 @@
-local g_GetTimeScale = game.GetTimeScale
-local m_Clamp = math.Clamp
---
 ---@param t EmitSoundInfo
-hook.Add("EntityEmitSound", "SCTOOLS_SoundPitchServer", function(t)
-  if not GetConVar("sc_change_sound_pitch"):GetBool() then return false end
-  local cheats = GetConVar("sv_cheats"):GetBool()
-  local host_timescale = GetConVar("host_timescale"):GetFloat()
-  local game_timescale = g_GetTimeScale()
+hook.Add("EntityEmitSound", "SCTOOLS_SoundPitchClient", function(t)
+  local cv_cheat = GetConVar("sv_cheats")
+  local cv_timescale = GetConVar("host_timescale")
+  local cv_sc_pitch = GetConVar("sc_change_sound_pitch")
+  if cv_sc_pitch == nil then return end
+
   local p = t.Pitch
-  if game_timescale ~= 1 then p = p * game_timescale end
-  if host_timescale ~= 1 and cheats then p = p * host_timescale end
-  if p ~= t.Pitch then
-    t.Pitch = m_Clamp(p, 0, 255)
+  if (game.GetTimeScale() ~= 1) then
+    p = p * game.GetTimeScale()
+  end
+  if (cv_timescale:GetFloat() ~= 1 and cv_cheat:GetBool()) then
+    p = p * cv_timescale:GetFloat()
+  end
+
+  if (p ~= t.Pitch and cv_sc_pitch:GetBool()) then
+    t.Pitch = math.Clamp(p, 0, 255)
     return true
   end
 end)
